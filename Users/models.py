efe7bc6aa2e353fa655b7ab1521421b0f54b7d90
saleh mohamed <114301423+Saleh-Moha@users.noreply.github.com>
2024-06-17 15:05:51 +0300

    Update models.py

--- Users/models.py
+++ Users/models.py
@@ -2,8 +2,8 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, Permis
 from django.db import models
 
 class CustomUserManager(BaseUserManager):
-
-    def create_user(self, email,username,first_name, last_name,bio,phone_number,location, date_of_birth,profile_photo, password=None, **extra_fields):
+    
+    def create_user(self, email, username, first_name, last_name, bio, phone_number, location, date_of_birth, is_private=False, password=None, **extra_fields):
         if not email:
             raise ValueError('The Email field must be set')
         email = self.normalize_email(email)
@@ -13,12 +13,12 @@ class CustomUserManager(BaseUserManager):
             first_name=first_name,
             last_name=last_name,
             date_of_birth=date_of_birth,
-            bio = bio,
+            bio=bio,
             phone_number=phone_number,
             location=location,
-            profile_photo=profile_photo,
+            is_private=is_private,
             **extra_fields
-        )        
+        )
         user.set_password(password)
         user.save(using=self._db)
         return user
@@ -32,11 +32,12 @@ class CustomUserManager(BaseUserManager):
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must have is_superuser=True.')
 
-        return self.create_user(email, password=password, **extra_fields)
+        return self.create_user(email, password=password, is_private=False, **extra_fields)
+
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
-    username = models.CharField(max_length=20,unique=True,blank=False)
+    username = models.CharField(max_length=20, unique=True, blank=False)
     bio = models.TextField(max_length=400)
     location = models.TextField(max_length=400)
     phone_number = models.IntegerField()
@@ -46,15 +47,17 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     date_joined = models.DateTimeField(auto_now_add=True)
-    profile_photo= models.ImageField(upload_to='src/usersprofilephotos')
+    profile_photo = models.ImageField(upload_to='src/usersprofilephotos',blank=True)
+    is_private = models.BooleanField(default=False, blank=True,null=False)
 
     objects = CustomUserManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name', 'last_name', 'date_of_birth','username','bio','location','phone_number']
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'date_of_birth', 'username', 'bio', 'location', 'phone_number']
 
     def __str__(self):
         return self.username
+
 
 class user_profile(models.Model):
     user = models.OneToOneField(CustomUser,on_delete=models.CASCADE)

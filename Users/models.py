170b976621d5068e105506a7fc27e62e360d5717
sale <sm3272692@gmail.com>
2024-06-21 16:32:17 +0300

    commit

--- Users/models.py
+++ Users/models.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
 
+from django.contrib.auth.models import BaseUserManager
+
 class CustomUserManager(BaseUserManager):
 
-    def create_user(self, email,username,first_name, last_name,bio,phone_number,location, date_of_birth,profile_photo, password=None, **extra_fields):
+    def create_user(self, email, username, first_name, last_name, date_of_birth, password=None, **extra_fields):
         if not email:
             raise ValueError('The Email field must be set')
         email = self.normalize_email(email)
@@ -13,17 +15,13 @@ class CustomUserManager(BaseUserManager):
             first_name=first_name,
             last_name=last_name,
             date_of_birth=date_of_birth,
-            bio = bio,
-            phone_number=phone_number,
-            location=location,
-            profile_photo=profile_photo,
             **extra_fields
-        )        
+        )
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password=None, **extra_fields):
+    def create_superuser(self, email, username, first_name, last_name, date_of_birth, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
 
@@ -32,29 +30,40 @@ class CustomUserManager(BaseUserManager):
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must have is_superuser=True.')
 
-        return self.create_user(email, password=password, **extra_fields)
+        return self.create_user(email, username, first_name, last_name, date_of_birth, password, **extra_fields)
+
+
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
-    username = models.CharField(max_length=20,unique=True,blank=False)
-    bio = models.TextField(max_length=400)
-    location = models.TextField(max_length=400)
-    phone_number = models.IntegerField()
-    first_name = models.CharField(max_length=30, blank=False)
-    last_name = models.CharField(max_length=30, blank=False)
-    date_of_birth = models.DateField(null=True, blank=False)
+    username = models.CharField(max_length=20, unique=True, blank=False)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+    date_of_birth = models.DateField(null=True, blank=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+    is_private = models.BooleanField(default=False)
     date_joined = models.DateTimeField(auto_now_add=True)
-    profile_photo= models.ImageField(upload_to='src/usersprofilephotos')
+  
 
     objects = CustomUserManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name', 'last_name', 'date_of_birth','username','bio','location','phone_number']
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'date_of_birth', 'username']
 
     def __str__(self):
         return self.username
 
+
 class user_profile(models.Model):
-    user = models.OneToOneField(CustomUser,on_delete=models.CASCADE)
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='user_profile')
+    profile_photo = models.ImageField(upload_to='src/usersprofilephotos', blank=True)
+    is_private = models.BooleanField(default=False, blank=True, null=False)
+    bio = models.TextField(max_length=400, blank=True)
+    location = models.TextField(max_length=400, blank=True)
+    phone_number = models.IntegerField(blank=True, null=True)  # Make it optional for testing
+
+    def __str__(self):
+        return str(self.user)
+
+
